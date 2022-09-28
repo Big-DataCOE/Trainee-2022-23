c41ea3a6f466daf098cee0f219c6ff2d7b720ed9
harshpanzer <harshpathakagra@gmail.com>
2022-09-28 17:48:57 +0530

    bit c

--- bitland.cpp
+++ bitland.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 int main(){
     int a,i,plus=0,minus=0;
-    string b;
-    cin>>a;
-    for(i=0;i<a;i++){
-        scanf("%s",&b);
-        if(b=="a++"||b=="++a")
+    int n; cin>>n;
+    for(i=0;i<n;i++){
+        string s;
+        cin>>s;
+        if(s=="X++"||s=="++X")
         plus++;
-        else if(b=="a--"||b=="--a")
+        else if(s=="X--"||s=="--X")
         minus++;
-        
+       
     }
     cout<<(plus-minus);
     return 0;

eeae493e06a6dc65a7730205ea94b59cebecf0f9
Utsav Singh <94159126+azutsavv@users.noreply.github.com>
2022-09-28 10:59:04 +0530

    Add files via upload

---
+++ bit++.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x=0;
+    cin>>n;
+    string s;
+    while(n--)
+    {
+        cin>>s;
+        if(s[1]=='+')
+        {
+            ++x;
+        }
+        else
+        --x;
+        
+    }
+    cout<<x;
+    return 0;
+}

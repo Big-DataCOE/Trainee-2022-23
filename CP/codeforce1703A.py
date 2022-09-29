d2ce9853df2eb62f374b2fe583127aa958589be0
Rani <rani2110010@akgec.ac.in>
2022-09-30 00:04:49 +0530

    v1.0.1

---
+++ CP/codeforce1703A.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(0,n):
+    p=input()
+    if((p[0]=="Y" or p[0]=="y") and(p[1]=="E" or p[1]=="e")and(p[1]=="E" or p[1]=="e")):
+        print("YES")
+    else :
+        print("NO")
+        

1c64ff44a7cff97c2d48cdc578661f0ffcc658fd
harshpanzer <harshpathakagra@gmail.com>
2022-09-30 16:41:20 +0530

    char2

--- charcount.cpp
+++ charcount.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include <bits/stdc++.h>
 #include<string.h>
+using namespace std;
 int main(){
     int a,c,i;
     char d;
@@ -8,8 +10,8 @@ int main(){
         std::string e;
         std::cin>>e;
         a=e.length();
-        if(a<10){
-            std::cout<< e;
+        if(a<=10){
+            std::cout<<e<<endl;
         }
         else{
             std::cout<<e[0]<<(a-2)<<e[a-1]<<"\n";

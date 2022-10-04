7bbb904d28ba2b91a4ea343dd70c1068c2db005c
Kushagr <kushagr2112057@akgec.ac.in>
2022-10-05 02:03:52 +0530

    Oct 5

--- MyPortfolio/app/src/main/java/com/example/myportfolio/myProjects.kt
+++ MyPortfolio/app/src/main/java/com/example/myportfolio/myProjects.kt
@@ -1,16 +1,19 @@
 package com.example.myportfolio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
-class profile : AppCompatActivity() {
+class myProjects : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_my_projects)
 
-        val back: ImageButton=findViewById(R.id.back)
-        back.setOnClickListener {
+        val backProjects: ImageButton =findViewById(R.id.backProject)
+        backProjects.setOnClickListener {
             finish()
         }
     }

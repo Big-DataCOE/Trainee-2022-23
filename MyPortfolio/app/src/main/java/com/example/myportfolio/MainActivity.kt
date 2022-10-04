a1d412945c216ab48f5eff3136a0530c5a4de17d
Kushagr <kushagr2112057@akgec.ac.in>
2022-10-04 10:08:24 +0530

    Oct 4

--- MyPortfolio/app/src/main/java/com/example/myportfolio/MainActivity.kt
+++ MyPortfolio/app/src/main/java/com/example/myportfolio/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.myportfolio
 
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val school: ImageButton= findViewById(R.id.school)
+        school.setOnClickListener{
+            val dialogBinding=layoutInflater.inflate(R.layout.schoolpopup,null)
+            val myDialog=Dialog(this)
+            myDialog.setContentView(dialogBinding)
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+        }
+        val college: ImageButton=findViewById(R.id.college)
+        college.setOnClickListener {
+            val dialogBinding1=layoutInflater.inflate(R.layout.collegepopup,null)
+            val myDialog1=Dialog(this)
+            myDialog1.setContentView(dialogBinding1)
+            myDialog1.setCancelable(true)
+            myDialog1.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog1.show()
+        }
+        val profilebtn: ImageButton=findViewById(R.id.profilebtn)
+        profilebtn.setOnClickListener {
+
+        }
+
     }
 }
---
+++ MyPortfolio/app/src/main/java/com/example/myportfolio/Profile.kt
@@ -0,0 +1,10 @@
+package com.example.myportfolio
+
+import android.os.Bundle
+
+class Profile {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile)
+    }
+}
